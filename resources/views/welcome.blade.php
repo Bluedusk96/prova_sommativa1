6fb5fd44c0c58436bfed1def5b3467f622e18401
Bluedusk96 <69deep96@gmail.com>
2016-03-23 20:16:31 +0100

    procedendo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,245 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+    <meta charset="utf-8">
+    
+  <!--   <title> Beauty Care Template | WIX</title>
+    <meta name="description" content="This template's earthy colors and natural theme make it the ideal match for your skincare or cosmetics company. Upload images of your products and customize the text to share your beauty philosophy. By adding items to their cart, customers can make purchases with ease.">
+    <meta name="author" content="Wixpress">
+    <meta http-equiv="content-language" content="it"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+
+    <meta property="og:title" content=" Beauty Care Template | WIX"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="http://it.wix.com/website-template/view/html/1629"/>
+    <meta property="og:image" content="http://static.wixstatic.com/media/3d4bae7650254c91aa0e103ad98f9d5b.jpg"/>
+    <meta content="Wix" property="og:site_name">
+    <meta property="og:description" content="This template's earthy colors and natural theme make it the ideal match for your skincare or cosmetics company. Upload images of your products and customize the text to share your beauty philosophy. By adding items to their cart, customers can make purchases with ease."/>
+    <meta property="fb:admins" content="731184828"/>
+    <meta name="fb:app_id" content="236335823061286"/>
+    <meta name="google-site-verification" content="QXhlrY-V2PWOmnGUb8no0L-fKzG48uJ5ozW0ukU7Rpo"/>
+
+        <link rel="canonical" href="http://it.wix.com/website-template/view/html/1629"/>
+
+            <link rel="alternate" hreflang="fr" href="http://fr.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="pt" href="http://pt.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="it" href="http://it.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="nl" href="http://nl.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="ko" href="http://ko.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="de" href="http://de.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="ru" href="http://ru.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="sv" href="http://sv.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="tr" href="http://tr.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="da" href="http://da.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="en" href="http://www.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="es" href="http://es.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="hi" href="http://hi.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="ja" href="http://ja.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="no" href="http://no.wix.com/website-template/view/html/1629"/>
+            <link rel="alternate" hreflang="pl" href="http://pl.wix.com/website-template/view/html/1629"/>
+        <link rel="alternate" hreflang="x-default" href="http://www.wix.com/website-template/view/html/1629" /> --> 
+
+
+    <!--CSS-->
+   <!--   <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.900.0/css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.900.0/new-create/css/fonts.css"/>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-public/1.185.0/css/Helvetica/fontFace.css"/>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.900.0/css/template_viewer.css"/>
+
+
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.900.0/css/ie9-and-down.css"/>
+    <![endif]-->
+
+    </head>
+<body lang="it">
+<!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MDD5C4"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+            {'gtm.start': new Date().getTime(),event:'gtm.js'}
+    );var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MDD5C4');</script>
+<!-- End Google Tag Manager -->
+    
+<script type="text/javascript">
+    var backUrl = "http:\/\/www.wix.com\/website\/templates";
+    var editUrl = "http://editor.wix.com/html/editor/web/renderer/new?metaSiteId=4389c816-d89e-409f-aaec-8436b34f14b0&siteId=c2db3e44-679d-494a-8d22-66248390a385";
+    var galleryDocIndex = "$pageModel.galleryDocIndex";
+    var originAppUrl = "$pageModel.unsafeJSoriginAppUrl";
+    var W = W || {};
+    W.App = W.App || {};
+    W.App.CLIENT_CONFIG = {"biServerUrl":"http://frog.wix.com/spettatore_bi?src=2&openingSiteId=c2db3e44-679d-494a-8d22-66248390a385&originAppUrl=http%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates&siteType=2"};
+    W.App.currentViewMode = 'desktop';
+
+    W.Globals = W.Globals || {};
+    W.Globals.lang =  "it";
+    var analytics = {"staging":false,"code":"UA-2117194-40","domain":"wix.com"};
+    var openAsTab = true;
+
+    //Storing dynamic text for responsive design
+    var headerTxt = {};
+    headerTxt.editButtonSmall = 'Modifica';
+    headerTxt.mainHeaderSmall = 'Crea il tuo sito spettacolare';
+</script>
+
+<!-- FACEBOOK CODE API-->
+<div id="fb-root"></div>
+<script>
+    /* Facebook Like Button*/
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    /*Google+ */
+    (function () {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+</script>
+<!-- End of FACEBOOK CODE -->
+
+<!--   <div id="container" class="template_viewer">
+    <div id="header">
+        <div class="navigation">
+            <div id="wixLogo">
+                <a href="http://it.wix.com/">Wix Press</a>
+            </div>
+                            <div id="template-mode-toggle">
+                    <div class="view-mode-toggle-button view-mode-toggle-button-desktop" data="desktop" title="Visualizzazione Principale"></div>
+                    <div class="view-mode-toggle-button view-mode-toggle-button-mobile" data="mobile" title="Visualizzazione Mobile"></div>
+                </div>
+                        <div class="templateDetails" itemscope itemtype="http://schema.org/Product/WebsiteTemplate">
+                <meta itemprop="name" content=" Beauty Care"/>
+            </div>
+            <!-- End of .templateDetails-->
+         <!--   <div class="templatesButtons">
+                <span id="templatesName">Clicca Modifica e crea il tuo fantastico sito</span> <a id="readMore"
+                                                                                                           href="#">Continua a leggere</a>
+                <meta itemprop="image" content="http://static.wixstatic.com/media/3d4bae7650254c91aa0e103ad98f9d5b.jpg"/>
+                <a href="#" class="button" id="editButton">Modifica questo sito<span>&nbsp;</span></a>
+                <span id="categoriesHolder">
+
+                </span>
+                <span class="priceTypeWrapper"> Prezzo: <span
+                        id="priceType">eCommerce</span></span>
+                <span class="html5icon"></span>
+
+                <div class="socialButtons">
+                    <div class="socialButtonsContent">
+                        <div class="facebookWrapper">
+                            <div class="fb-like" data-send="false" data-layout="button_count"
+                                 data-show-faces="true"></div>
+                        </div>
+                        <div class="googlePlusWrapper">
+                            <g:plusone size="medium"></g:plusone>
+                        </div>
+                    </div> -->
+                    <!-- end of .socialButtonsContent -->
+                </div>
+                <!-- end of .socialButtons -->
+            </div>
+        </div>
+        <!-- End of .navigation-->
+    </div>
+    <!-- End of #Header-->
+    <div id="headerShadows">
+        <div class="rightShadow"></div>
+        <div class="leftShadow"></div>
+    </div>
+    <!-- End of #HeaderShadows-->
+    <div class="contentWrapper">
+                                    <div class="preview-tab preview-tab-desktop">
+                <iframe src="http://it.wix.com/demone2/beauty-care-1" width="100%" height="100%"></iframe>
+            </div>
+        
+        <!--</div>--><!-- End of .content-->
+   </div>
+    <!-- End of .contentWrapper-->
+
+   <!--   <div id="dialog-overlay"></div>
+    <div id="dialog-box">
+        <a id="close-pop-up-button" href="#">Close Pop Up Button</a>
+        <div class="dialog-content">
+            <h1 class="dialog-title">&#8216; Beauty Care&#8217; - Website Template</h1>
+            <div class="leftColumn">
+                <h3>Va bene per:</h3>
+
+                <div id="dialog-good-for">Spas, natural stores and organic product stockists.</div>
+                <h3>Descrizione:</h3>
+
+                <div id="dialog-description" itemprop="description">This template's earthy colors and natural theme make it the ideal match for your skincare or cosmetics company. Upload images of your products and customize the text to share your beauty philosophy. By adding items to their cart, customers can make purchases with ease.</div>
+            </div>
+            <div class="rightColumn">
+                                                                <span>
+					<h3>Difficoltà:&nbsp;</h3>
+					<div id="dialog-difficulty" itemprop="difficulty">Medium</div>
+				</span>
+				<span itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+					<h3>Prezzo:&nbsp;</h3>
+					<div id="dialog-price" itemprop="price">eCommerce</div>
+					<link itemprop="availability" href="http://schema.org/InStock"/>
+				</span>
+                <a href="#" id="dialog-edit-now">Modifica ora</a>
+            </div>
+            <div class="clearBoth">&nbsp;</div>
+        </div>
+        <div class="dialog-box-shadows">
+            <div class="leftShadow"></div>
+            <div class="rightShadow"></div>
+        </div>
+    </div> --> 
+    <!-- End of #dialog-box-->
+
+</div>
+<!-- End of #container-->
+
+<!--Scripts after page loads-->
+<script src="http://static.wix.com/services/third-party/jquery/1.4.3/min/jquery.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://static.parastorage.com/services/wix-html-template-viewer/1.900.0/js/components/scrollbar/custom-scrollbar.js"></script>
+<script type="text/javascript" src="http://static.parastorage.com/services/wix-html-template-viewer/1.900.0/js/common.js"></script>
+<script src="http://static.parastorage.com/services/wix-html-template-viewer/1.900.0/js/template_viewer.js" type="text/javascript"></script>
+
+        
+
+<!--   <div style="visibility: hidden">view_template_v.vm</div> -->
+
+
+<!--  <script type="text/javascript">
+            window.templateEditUrl = 'http://editor.wix.com/html/editor/web/renderer/new?metaSiteId=4389c816-d89e-409f-aaec-8436b34f14b0&siteId=c2db3e44-679d-494a-8d22-66248390a385';
+    </script>
+<script type="text/javascript">
+//<![CDATA[
+(function() {
+var _analytics_scr = document.createElement('script');
+_analytics_scr.type = 'text/javascript'; _analytics_scr.async = true; _analytics_scr.src = '/_Incapsula_Resource?SWJIYLWA=2977d8d74f63d7f8fedbea018b7a1d05&ns=1';
+var _analytics_elem = document.getElementsByTagName('script')[0]; _analytics_elem.parentNode.insertBefore(_analytics_scr, _analytics_elem);
+})();
+// ]]>
+</script></body>
+</html>
+
+
+
+
+
+
+
+
+
+<!--  <!DOCTYPE html>
 <html>
     <head>
         <title>Laravel</title>
@@ -42,4 +283,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> -->
